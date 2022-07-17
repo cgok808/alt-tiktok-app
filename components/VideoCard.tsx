36cca0a5dcff08ddacf235db2b58d1e3b0f89a03
Chay <chaylin808@gmail.com>
2022-07-18 07:15:15 +0900

    upload video functionality

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -65,7 +65,7 @@ const VideoCard: NextPage<IProps> = ({ post }: IProps) => {
           onMouseLeave={() => setIsHover(false)}
           className='rounded-3xl'
         >
-          <Link href='/'>
+          <Link href={`/detail/${post._id}`}>
             <video
               loop
               ref={videoRef}
@@ -74,7 +74,7 @@ const VideoCard: NextPage<IProps> = ({ post }: IProps) => {
             ></video>
           </Link>
           {isHover && (
-            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3">
               {playing ? (
                 <button onClick={onVideoPress}>
                   <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
