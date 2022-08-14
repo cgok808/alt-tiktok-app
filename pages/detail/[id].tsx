f74f91adc09dafac7332deb0bb7631844330c9f5
Chay <chaylin.gokan@gmail.com>
2022-08-14 03:38:43 -0400

    comment implementation

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -25,6 +25,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const { userProfile }: any = useAuthStore();
+  const [comment, setComment] = useState("");
+  const [isPostingComment, setIsPostingComment] = useState(false);
 
   const onVideoClick = () => {
     if (playing) {
@@ -50,6 +52,22 @@ const Detail = ({ postDetails }: IProps) => {
         like,
       });
       setPost({ ...post, likes: data.likes });
+    }
+  };
+
+  const addComment = async (e) => {
+    e.preventDefault();
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+
+      setPost({ ...post, comments: data.comments });
+      setComment("");
+      setIsPostingComment(false);
     }
   };
 
@@ -127,13 +145,19 @@ const Detail = ({ postDetails }: IProps) => {
         <div className='mt-10 px-10'>
           {userProfile && (
             <LikeButton
-            likes={post.likes}
+              likes={post.likes}
               handleLike={() => handleLike(true)}
               handleDislike={() => handleLike(false)}
             />
           )}
         </div>
-        <Comments />
+        <Comments
+          comment={comment}
+          setComment={setComment}
+          addComment={addComment}
+          comments={post.comments}
+          isPostingComment={isPostingComment}
+        />
       </div>
     </div>
   );
